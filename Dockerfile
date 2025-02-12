653becafd0c65d9b885e48889f0c28111236c2ec
devsjc <47188100+devsjc@users.noreply.github.com>
2025-02-12 11:34:34 +0000

    feat(repo): Add structured logging for containers (#13)

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.11-slim
 COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
 
 RUN apt-get update -y && \
-    apt-get -y --no-install-recommends install git unzip build-essential && \
+    apt-get -y --no-install-recommends install git build-essential && \
     rm -rf /var/lib/apt/lists/*
 
 # Add repository code
@@ -12,6 +12,8 @@ COPY pyproject.toml /opt/app
 COPY .git /opt/app/.git
 
 RUN uv sync --no-dev
+
+ENV _TYPER_STANDARD_TRACEBACK=1
 
 ENTRYPOINT ["uv", "run", "--no-dev"]
 CMD ["cloudcasting-app"]
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
 import os
-import pytest
 
+import fsspec
 import numpy as np
 import pandas as pd
+import pytest
 import xarray as xr
-import fsspec
-
 
 xr.set_options(keep_attrs=True)
 
@@ -15,16 +14,16 @@ def test_t0():
 
 
 def make_sat_data(test_t0, freq_mins):
-    
+
     # Load dataset which only contains coordinates, but no data
     shell_path = f"{os.path.dirname(os.path.abspath(__file__))}/test_data/non_hrv_shell.zarr.zip"
-    
+
     ds = xr.open_zarr(fsspec.get_mapper(f"zip::{shell_path}"))
 
-    # Remove original time dim 
+    # Remove original time dim
     ds = ds.drop_vars("time")
 
-    # Add new times so they lead up to present 
+    # Add new times so they lead up to present
     times = pd.date_range(
         test_t0 - pd.Timedelta("3h"),
         test_t0,
@@ -37,7 +36,7 @@ def make_sat_data(test_t0, freq_mins):
         np.zeros([len(ds[c]) for c in ds.xindexes]),
         coords=[ds[c] for c in ds.xindexes],
     )
-    
+
     # Add stored attributes to DataArray
     ds.data.attrs = ds.attrs["_data_attrs"]
     del ds.attrs["_data_attrs"]
